4ff3ba3b2405ef68a47b7c1d64e672794b06df60
unknown <marianaalmada15@outlook.com>
2022-05-17 23:03:01 -0300

    estados

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,19 +5,40 @@ class Counter extends Component {
 /**
  * mediante eventos se va a ir cambiando el valor del estado del componente
  */
+    constructor(props) {
+        super(props);
+        this.state = {valor: 0};
 
-    state = {
-        valor: 0
+        this.decrementCounter = this.decrementCounter.bind(this);
+        //this.incrementCounter = this.incrementCounter.bind(this);
     }
 
     /*se debe llamar a la función dentro del bind dentro constructor*/
 
+    decrementCounter() {
+        this.setState({
+            valor: this.state.valor - 1
+        });
+    }
+
+    incrementCounter() {
+        this.setState({
+            valor: this.state.valor + 1
+        });
+    }
+
+    incrementar = () => {
+        this.setState({
+            valor: this.state.valor + 1
+        });
+    }
+
     render() {
         return (
             <div>
-                <button onClick={this.decrement}>-</button>
+                <button onClick={this.decrementCounter} >-</button>
                 <p>{this.state.valor}</p>
-                <button>+</button>
+                <button onClick={this.incrementar}>+</button>
             </div>
         )
     }
@@ -25,6 +46,6 @@ class Counter extends Component {
 
 export default Counter
 
-/*setState cambia el estaoo a partir de un evento.
+/*setState cambia el estado a partir de un evento.
 El estado se puede pasar a un componente hijo
 * mediante hooks ahora se puede controlar el estado de componentes funcionales*/
